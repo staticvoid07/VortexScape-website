a5ffb490db12b85d33ba76b511cc5ba942a97576
staticvoid07 <static.v0id2000@gmail.com>
2025-12-23 18:29:59 -0300

    inverted patch notes order

--- index.php
+++ index.php
@@ -74,30 +74,48 @@
     
     <div class="patch-notes-section">
         <h2>Patch notes</h2>
-        <div class="patch-version">Dec. 11th 2025</div>
+        <div class="patch-version">Dec. 23rd 2025</div>
         <ul class="patch-list">
-            <li>removed prayer drain</li>
-            <li>removed the waiting period to sell items to vendors after you steal from their stall</li>
-            <li>changes to aggro npc list (scroll down for full list)</li>
-            <li>completing rune mysteries with Hans now gives an air talisman</li>
-            <li>increased thieving by 25 on pickpocket success checks</li>
-            <li>you can now enter the waterfall dungeon without glarial's amulet if you already completed the quest</li>
-            <li>all npc drops will drop into the inventory</li>
-            <li>father aereck will give you an incinerator ring if you have 100 quest points</li>
-            <li>having the incinerator ring equipped or in the inventory will incinerate bones for 100% prayer xp</li>
+            <li>added blue dragon scales to druid pickpocket table</li>
         </ul>
-        <div class="patch-version">Dec. 12th 2025</div>
+        <div class="patch-version">Dec. 21st 2025</div>
         <ul class="patch-list">
-            <li>fixed the skilling interruption bug (woodcutting, thieving stalls, smelting, and others used to get interrupted randomly)</li>
-            <li>changed dark wizards' drop table to give herblore secondaries, as there is no good way of afking secondaries. THIS CHANGE IS NOW OBSOLETE, check Dec. 17th</li>
-            <li>added more npcs that are infinitely aggressive and hunt range of 5, and respawn rate of 5 ticks (scroll down for npc list)</li>
+            <li>fixed weapon unequipping game crash</li>
+            <li>guards and stall owners no longer aggro when stealing from stalls</li>
         </ul>
-        <div class="patch-version">Dec. 13th 2025</div>
+        <div class="patch-version">Dec. 20th 2025</div>
         <ul class="patch-list">
-            <li>changed buy-50 to buy-250</li>
-            <li>added a sell-5000 option</li>
-            <li>fixed charged jewellery deleting the entire stack instead of just one when you used a charge</li>
-            <li>mage arena capes drop to the inventory (to fix a game crash)</li>
+            <li>decreased thieving delays of stalls to make them a viable idle method</li>
+            <li>removed the ability to click thieving stalls repeatedly for faster xp</li>
+            <li>ranged ammo will automatically be picked up after it's used</li>
+            <li>changed crafting and herblore skill delays to match osrs, as they had no delays in 2004</li>
+            <li>deployed more changes from this revision, including troll stronghold, law runecrafting and KQ</li>
+            <li>Hans can now complete every quest available in the quest journal</li>
+        </ul>
+        <div class="patch-version">Dec. 17th 2025</div>
+        <ul class="patch-list">
+            <li>removed dark wizards from aggro npc list</li>
+            <li>dark wizards no longer drop herblore secondaries</li>
+            <li>added four chaos druids to the upper floor of the chaos druid tower</li>
+            <li>added pickpocketing druids with a thieving level requirement of 38</li>
+            <li>pickpocketing druids gives herblore secondaries</li>
+            <li>essence mining changed to 2 essence per mine on average at 85 or higher runecraft</li>
+            <li>essence mining changed to 2.5 essence per mine on average at 99 runecraft, to match daeyalt mining rates on osrs</li>
+        </ul>
+        <div class="patch-version">Dec. 16th 2025</div>
+        <ul class="patch-list">
+            <li>you now mine 3 rune essence at a time at level 85 runecraft, and 5 at a time at 99. THIS CHANGE IS NOW OBSOLETE, check Dec. 17th</li>
+            <li>added highscores</li>
+            <li>removed profanity filter from java client</li>
+        </ul>
+        <div class="patch-version">Dec. 15th 2025</div>
+        <ul class="patch-list">
+            <li>changed shop logic to allow buying any amount of anything that is in stock</li>
+            <li>removed spam click alchemy spells</li>
+            <li>removed spam click superheating</li>
+            <li>combat boosts never tick down</li>
+            <li>family crest npcs will give you their gauntlet after quest completion</li>
+            <li>removed bronze bars from all shop stocks</li>
         </ul>
         <div class="patch-version">Dec. 14th 2025</div>
         <ul class="patch-list">
@@ -116,48 +134,30 @@
             <li>fixed amulet of glory deleting entire stack when using a charge</li>
             <li>fixed charging glories turning the whole stack into one</li>
         </ul>
-        <div class="patch-version">Dec. 15th 2025</div>
+        <div class="patch-version">Dec. 13th 2025</div>
         <ul class="patch-list">
-            <li>changed shop logic to allow buying any amount of anything that is in stock</li>
-            <li>removed spam click alchemy spells</li>
-            <li>removed spam click superheating</li>
-            <li>combat boosts never tick down</li>
-            <li>family crest npcs will give you their gauntlet after quest completion</li>
-            <li>removed bronze bars from all shop stocks</li>
+            <li>changed buy-50 to buy-250</li>
+            <li>added a sell-5000 option</li>
+            <li>fixed charged jewellery deleting the entire stack instead of just one when you used a charge</li>
+            <li>mage arena capes drop to the inventory (to fix a game crash)</li>
         </ul>
-        <div class="patch-version">Dec. 16th 2025</div>
+        <div class="patch-version">Dec. 12th 2025</div>
         <ul class="patch-list">
-            <li>you now mine 3 rune essence at a time at level 85 runecraft, and 5 at a time at 99. THIS CHANGE IS NOW OBSOLETE, check Dec. 17th</li>
-            <li>added highscores</li>
-            <li>removed profanity filter from java client</li>
+            <li>fixed the skilling interruption bug (woodcutting, thieving stalls, smelting, and others used to get interrupted randomly)</li>
+            <li>changed dark wizards' drop table to give herblore secondaries, as there is no good way of afking secondaries. THIS CHANGE IS NOW OBSOLETE, check Dec. 17th</li>
+            <li>added more npcs that are infinitely aggressive and hunt range of 5, and respawn rate of 5 ticks (scroll down for npc list)</li>
         </ul>
-        <div class="patch-version">Dec. 17th 2025</div>
+        <div class="patch-version">Dec. 11th 2025</div>
         <ul class="patch-list">
-            <li>removed dark wizards from aggro npc list</li>
-            <li>dark wizards no longer drop herblore secondaries</li>
-            <li>added four chaos druids to the upper floor of the chaos druid tower</li>
-            <li>added pickpocketing druids with a thieving level requirement of 38</li>
-            <li>pickpocketing druids gives herblore secondaries</li>
-            <li>essence mining changed to 2 essence per mine on average at 85 or higher runecraft</li>
-            <li>essence mining changed to 2.5 essence per mine on average at 99 runecraft, to match daeyalt mining rates on osrs</li>
-        </ul>
-        <div class="patch-version">Dec. 20th 2025</div>
-        <ul class="patch-list">
-            <li>decreased thieving delays of stalls to make them a viable idle method</li>
-            <li>removed the ability to click thieving stalls repeatedly for faster xp</li>
-            <li>ranged ammo will automatically be picked up after it's used</li>
-            <li>changed crafting and herblore skill delays to match osrs, as they had no delays in 2004</li>
-            <li>deployed more changes from this revision, including troll stronghold, law runecrafting and KQ</li>
-            <li>Hans can now complete every quest available in the quest journal</li>
-        </ul>
-        <div class="patch-version">Dec. 21st 2025</div>
-        <ul class="patch-list">
-            <li>fixed weapon unequipping game crash</li>
-            <li>guards and stall owners no longer aggro when stealing from stalls</li>
-        </ul>
-        <div class="patch-version">Dec. 23rd 2025</div>
-        <ul class="patch-list">
-            <li>added blue dragon scales to druid pickpocket table</li>
+            <li>removed prayer drain</li>
+            <li>removed the waiting period to sell items to vendors after you steal from their stall</li>
+            <li>changes to aggro npc list (scroll down for full list)</li>
+            <li>completing rune mysteries with Hans now gives an air talisman</li>
+            <li>increased thieving by 25 on pickpocket success checks</li>
+            <li>you can now enter the waterfall dungeon without glarial's amulet if you already completed the quest</li>
+            <li>all npc drops will drop into the inventory</li>
+            <li>father aereck will give you an incinerator ring if you have 100 quest points</li>
+            <li>having the incinerator ring equipped or in the inventory will incinerate bones for 100% prayer xp</li>
         </ul>
         <h2>Aggressive NPC list</h2>
         <ul class="patch-list">
